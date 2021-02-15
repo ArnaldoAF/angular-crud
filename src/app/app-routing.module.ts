05b57e799f6dbf5d7a6e016f9e8615a8c001bb9f
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2021-02-14 21:48:19 -0300

    HomePage and Header/Routes

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+
+const routes: Routes = [
+    {
+        path: "",
+        component: HomepageComponent
+    },
+    {
+        path: "home",
+        component: HomepageComponent
+    },
+    
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
